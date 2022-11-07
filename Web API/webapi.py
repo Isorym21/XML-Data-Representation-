5d393ab20ea688783ee4c06a8487d10dc0c69dd9
Isory Santana <103542108+Isorym21@users.noreply.github.com>
2022-11-07 14:50:19 -0500

    Add files via upload

---
+++ Web API/webapi.py
@@ -0,0 +1,28 @@
+import social_security
+import marginal_tax
+
+from flask import Flask, request
+
+#pip install Flask 
+
+app = Flask(__name__)
+
+
+@app.route('/payments')
+def get_ss_info():
+    rate = request.args.get("rate", default=.0, type=float)
+
+
+    results = social_security.comparison(rate=rate)
+    map_results = {"developer" : "Santana, Isory", "results": results, "rate": rate }
+    return map_results
+
+@app.route('/taxsummary')
+def get_tax_summary():
+    s_code= request.args.get("code", default= "SM", type=str)
+    n_salary= request.args.get("salary=", default= 10_275.0, type=float)
+    tax_summary = marginal_tax.get_tax_paid_summary(s_code, n_salary)
+    return tax_summary
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
